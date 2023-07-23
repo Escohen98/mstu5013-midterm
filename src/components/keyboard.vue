4cc096dee1183757843f72f9da440287130c401d
Eric <you@example.com>
2023-07-23 14:51:49 -0400

    Adding project

---
+++ src/components/keyboard.vue
@@ -0,0 +1,32 @@
+<template>
+    <img class="key" v-bind:id="{keyNum}" v-bind:href="{keyImage}"/>
+    <audio>
+        <source v-bind:src="{keyAudio}">
+    </audio>
+</template>
+
+<script>
+export default {
+    name: 'key',
+    data() {
+        return {
+            keyNum: Number,
+            keyImage: String, //Default image
+            keyHoverImage: String, //Image on hover
+            keyAudio: String, //Audio on click
+            keyRef: String //Ref to play the audio
+        };
+    }
+}
+</script>
+
+<style scoped>
+    .key {
+        background-color: white; /*Default key */
+        border: 2px solid black;
+    }
+
+    .key:hover {
+        box-shadow: 4px rgba(255, 255, 0, .1);
+    }
+</style>
